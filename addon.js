70ebcfe809b4e5298225a6441c0055eab9be558d
hostitorrent <host.itorrent@gmail.com>
2022-03-10 18:24:10 +0100

    main

--- addon.js
+++ addon.js
@@ -42,7 +42,7 @@ const dataset = {
     "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
     "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
 
-    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
+    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://192.168.1.164:6878/content/cef832d88e4f1ef6590ca15a284657debd84c793/0.957227019732" }, // HTTP stream
     "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
 
     "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
