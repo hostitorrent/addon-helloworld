8f95c333ec76736e0cadb9a481d2b337771feceb
hostitorrent <host.itorrent@gmail.com>
2022-03-11 08:09:04 +0100

    main

--- addon.js
+++ addon.js
@@ -1,113 +1,76 @@
 const { addonBuilder } = require("stremio-addon-sdk");
-const magnet = require("magnet-uri");
+const fs = require('fs');
 
-const manifest = { 
-    "id": "org.stremio.helloworld",
-    "version": "1.0.0",
-
-    "name": "Hello World Addon",
-    "description": "Sample addon providing a few public domain movies",
-
-    // set what type of resources we will return
+const manifest = {
+    "id": "org.stremio.oscarmovies",
+    "version": "1.0.4",
+    "name": "Oscar Movies",
+    "description": "Oscar Nominees of the year",
+    //"icon": "URL to 256x256 monochrome png icon",
+    //"background": "URL to 1024x786 png/jpg background",
     "resources": [
         "catalog",
         "stream"
     ],
-
-    "types": ["movie", "series"], // your add-on will be preferred for those content types
-
-    // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+    "types": ["movie", "series"],
     "catalogs": [
         {
             type: 'movie',
-            id: 'helloworldmovies'
+            id: 'oscarmovies'
         },
         {
-            type: 'series',
-            id: 'helloworldseries'
+            type: 'Sinkronos Filmek',
+            id: 'oscaranimated'
+        },
+        {
+            type: 'documentary',
+            id: 'oscardocumentary'
+        },
+        {
+            type: 'shortfilm',
+            id: 'oscarshortfilm'
         }
     ],
-
-    // prefix of item IDs (ie: "tt0032138")
-    "idPrefixes": [ "tt" ]
-
+    "idPrefixes": ["tt"] // prefix of item IDs (ie: "tt0032138")
 };
-
-const dataset = {
-    // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
-    "tt0032138": { name: "The Wizard of Oz", type: "movie", genres:[ "Akcio","Fantasy"], infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0, trailers:[{"source":"_tT2_76ZcSU","type":"trailers"}], description: "Kansason félelmetes erejű tornádó söpör végig. A kis Dorothy nem tud elmenekülni, balesetet szenved. Ájult állapotban kis kutyájával, Totóval csodálatos helyre, Óz birodalmába kerül. Útja során barátokat szerez az Oroszlán, a Madárijesztő és a Bádogember személyében, ám egy kegyetlen ellenséggel, a Boszorkánnyal is szembe kerül. Noha itt minden színes és szép, Dorothy legfőbb vágya, hogy visszajusson az ő fekete-fehér, mégis úgy szeretett otthonába..", imdbRating: "6.9", releaseInfo: '2008', runtime: ["30 sec"], director: ["Victor Fleming"], cast: ["Judy Garland,Frank Morgan,Ray Bolger"], }, 
-    "tt0017136": { name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
-
-    // night of the living dead, example from magnet
-    "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
-    "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
-
-    "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://192.168.1.164:6878/content/cef832d88e4f1ef6590ca15a284657debd84c793/0.957227019732" }, // HTTP stream
-    "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
-
-    "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
-
-    "tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
-};
-
-// utility function to add from magnet
-function fromMagnet(name, type, uri) {
-    const parsed = magnet.decode(uri);
-    const infoHash = parsed.infoHash.toLowerCase();
-    const tags = [];
-    if (uri.match(/720p/i)) tags.push("720p");
-    if (uri.match(/1080p/i)) tags.push("1080p");
-    return {
-        name: name,
-        type: type,
-        infoHash: infoHash,
-        sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
-        tag: tags,
-        title: tags[0], // show quality in the UI
-    }
-}
 
 const builder = new addonBuilder(manifest);
 
-// Streams handler
-builder.defineStreamHandler(function(args) {
+let JsonData = fs.readFileSync('./dataset/dataset5.json');
+let dataset = JSON.parse(JsonData);
+
+builder.defineStreamHandler(function (args) {
     if (dataset[args.id]) {
-        return Promise.resolve({ streams: [dataset[args.id]] });
+        return Promise.resolve({streams: [dataset[args.id]]});
     } else {
-        return Promise.resolve({ streams: [] });
+        return Promise.resolve({streams: []});
     }
 })
 
-const METAHUB_URL = "https://images.metahub.space"
 
-const generateMetaPreview = function(value, key) {
-    // To provide basic meta for our movies for the catalog
-    // we'll fetch the poster from Stremio's MetaHub
-    // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
+const generateMetaPreview = function (value, key) {
     const imdbId = key.split(":")[0]
     return {
         id: imdbId,
-        type: value.type,
-        name: value.name,
-        genres: value.genres,
-        description: value.description,
+        type: value.Type,
+        name: value.Title,
+        description: value.Plot,
+        director: [value.Director],
+        cast: value.Actors.split(','),
+        genres: value.Genre.split(','),
+        releaseInfo: value.Year,
         imdbRating: value.imdbRating,
-        releaseInfo: value.releaseInfo,
-        runtime: value.runtime,
-        director: value.director,
-        cast: value.cast,
-        trailers: value.trailers,
-        poster: METAHUB_URL+"/poster/medium/"+imdbId+"/img",
+        trailers: value.Trailer,
+        poster: value.Poster
     }
 }
 
-builder.defineCatalogHandler(function(args, cb) {
-    // filter the dataset object and only take the requested type
+builder.defineCatalogHandler(function (args, cb) {
     const metas = Object.entries(dataset)
-	.filter(([_, value]) => value.type === args.type)
-	.map(([key, value]) => generateMetaPreview(value, key))
+        .filter(([_, value]) => value.Type === args.type)
+        .map(([key, value]) => generateMetaPreview(value, key))
 
-    return Promise.resolve({ metas: metas })
+    return Promise.resolve({metas: metas})
 })
 
 module.exports = builder.getInterface()
